1722d354bb8a6bed8a9d9702bcce67ffa82027a9
Adrián Manzano <410541adrianmanzano@gmail.com>
2021-10-26 21:02:28 -0300

    inicio con sprint

---
+++ practicartodo/src/main/java/com/example/practicartodo/PracticartodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicartodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticartodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticartodoApplication.class, args);
+	}
+
+}
---
+++ practicartodo/src/test/java/com/example/practicartodo/PracticartodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicartodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticartodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
